2ef02ef6433dfe24cbab2ff0d058719845af2203
QuaternionStudios <butakovoleg@gmail.com>
2014-06-26 13:16:12 +0400

    Created Atomic operator interface/implementations

---
+++ source/GoddamnEngine/Core/Threading/Atomic/Atomic.WinApi.hh
@@ -0,0 +1,84 @@
+//////////////////////////////////////////////////////////////////////////
+/// Atomic.WinApi.hh - Atomics basics implementations for WinApi.
+/// Copyright (C) $(GODDAMN_DEV) 2011 - Present. All Rights Reserved.
+/// 
+/// History:
+///		* 29.04.2014 - Created by James Jhuighuy
+//////////////////////////////////////////////////////////////////////////
+
+#include <GoddamnEngine/Include.hh>
+
+#pragma once 
+#ifndef GD_CORE_THREADING_ATOMIC_WINAPI
+#define GD_CORE_THREADING_ATOMIC_WINAPI
+
+#if (!defined(GD_CORE_THREADING_ATOMIC))
+#	error 'Please, do not include 'Atomic.WinApi.hh' directly, use 'Atomic.hh' instead.'
+#endif	// if (!defined(GD_CORE_THREADING_ATOMIC))
+
+GD_NAMESPACE_BEGIN
+	namespace Atomics
+	{
+		GDINL static Int32 InterlockedIncrement(Int32 volatile* const Value)
+		{
+			return static_cast<Int32>(::InterlockedIncrement(reinterpret_cast<LPLONG>(const_cast<Int32*>(Value))));
+		}
+
+		GDINL static Int64 InterlockedIncrement(Int64 volatile* const Value)
+		{
+			return static_cast<Int64>(::InterlockedIncrement64(reinterpret_cast<LONGLONG*>(const_cast<Int64*>(Value))));
+		}
+
+		GDINL static Int32 InterlockedDecrement(Int32 volatile* const Value)
+		{
+			return static_cast<Int32>(::InterlockedDecrement(reinterpret_cast<LPLONG>(const_cast<Int32*>(Value))));
+		}
+
+		GDINL static Int64 InterlockedDecrement(Int64 volatile* const Value)
+		{
+			return static_cast<Int64>(::InterlockedDecrement64(reinterpret_cast<LONGLONG*>(const_cast<Int64*>(Value))));
+		}
+
+		GDINL static Int32 InterlockedAdd(Int32 volatile* const Value, Int32 const Amount)
+		{
+			return static_cast<Int32>(::InterlockedExchangeAdd(reinterpret_cast<LPLONG>(const_cast<Int32*>(Value)), static_cast<LONG>(Amount)));
+		}
+
+		GDINL static Int64 InterlockedAdd(Int64 volatile* const Value, Int64 const Amount)
+		{
+			return static_cast<Int64>(::InterlockedExchangeAdd64(reinterpret_cast<LONGLONG*>(const_cast<Int64*>(Value)), static_cast<LONGLONG>(Amount)));
+		}
+
+		GDINL static Int32 InterlockedExchange(Int32 volatile* const Value, Int32 const Other)
+		{
+			return static_cast<Int32>(::InterlockedExchange(reinterpret_cast<LPLONG>(const_cast<Int32*>(Value)), static_cast<LONG>(Other)));
+		}
+
+		GDINL static Int64 InterlockedExchange(Int64 volatile* const Value, Int64 const Other)
+		{
+			return static_cast<Int64>(::InterlockedExchange64(reinterpret_cast<LONGLONG*>(const_cast<Int64*>(Value)), static_cast<LONGLONG>(Other)));
+		}
+
+		GDINL static handle InterlockedExchangePtr(handle* const Destination, handle const Other)
+		{
+			return ::InterlockedExchangePointer(Destination, Other);
+		}
+
+		GDINL static Int32 InterlockedCompareExchange(Int32 volatile* const Destination, Int32 const Other, Int32 const Comparand)
+		{
+			return static_cast<Int32>(::InterlockedCompareExchange(reinterpret_cast<LPLONG>(const_cast<Int32*>(Destination)), static_cast<LONG>(Other), static_cast<LONG>(Comparand)));
+		}
+
+		GDINL static Int64 InterlockedCompareExchange(Int64 volatile* const Destination, Int64 const Other, Int64 const Comparand)
+		{
+			return static_cast<Int64>(::InterlockedCompareExchange64(reinterpret_cast<LONGLONG*>(const_cast<Int64*>(Destination)), static_cast<LONGLONG>(Other), static_cast<LONGLONG>(Comparand)));
+		}
+
+		GDINL static handle InterlockedCompareExchangePtr(handle* const Destination, handle const Other, handle const Comparand)
+		{
+			return ::InterlockedCompareExchangePointer(Destination, Other, Comparand);
+		}
+	}	// namespace Atomics
+GD_NAMESPACE_END
+
+#endif
--- source/GoddamnEngine/Core/Threading/Atomic/Atomic.hh
+++ source/GoddamnEngine/Core/Threading/Atomic/Atomic.hh
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////////////
-/// Atomic.hh - Atomic variables interface and implementation.
+/// Atomic.hh - Atomics variables interface and implementation.
 /// Copyright (C) $(GODDAMN_DEV) 2011 - Present. All Rights Reserved.
 /// 
 /// History:
@@ -11,64 +11,76 @@
 #define GD_CORE_THREADING_ATOMIC
 
 #include <GoddamnEngine/Include.hh>
+#include <GoddamnEngine/Core/TypeTraits.hh>
 #include <GoddamnEngine/Core/Diagnostics/Assertion/Assertion.hh>
-#if (defined(GD_PLATFORM_WINDOWS) || defined(GD_PLATFORM_XBOX_ONE) || defined(GD_PLATFORM_WINPNONE8))
-#	define GD_THREAD_WINAPI		1
-#	define WIN32_LEAN_AND_MEAN
-#	include <Windows.h>
-#else	// if (defined(GD_PLATFORM_WINDOWS) || defined(GD_PLATFORM_XBOX_ONE) || defined(GD_PLATFORM_WINPNONE8))
-#	define GD_THREAD_PTHREAD	1
-#	include <pthread.h>
-#endif	// if (defined(GD_PLATFORM_WINDOWS) || defined(GD_PLATFORM_XBOX_ONE) || defined(GD_PLATFORM_WINPNONE8))
-
-GD_NAMESPACE_BEGIN
-
-	/// Defines a 64 bit signed counter that can be safely used across threads.
-	template<class ElementType>
-	class Atomic64 final
-	{
-	private:
-		static_assert((sizeof(ElementType) <= sizeof(Int64)), "Atomic class allows storing only 64-bit sized (or less types)");
-		union 
-		{
-			ElementType volatile Wrapped;
-			Int64       volatile Value = 0;
-		} volatile;
-
-	public:
-		GDINL Atomic64(Int64  const  Value) : Value(Value) { }
-		GDINL Atomic64(Atomic64 const& OtherAtomic) : Value(OtherAtomic.Value) { }
 
 #if (defined(GD_DOCUMENTATION))
-		/// Increments value of counter and returns it to caller.
-		GDINL Int64 Increment();
-		/// Decrements value of counter and returns it to caller.
-		GDINL Int64 Decrement();
-		/// Changes counter value to specified one returns old value to caller.
-		GDINL Int64 Exchange(Int64 const NewValue);
-#else	// if (defined(GD_DOCUMENTATION))
-#	if (defined(GD_PLATFORM_WINDOWS) || defined(GD_PLATFORM_XBOX_ONE) || defined(GD_PLATFORM_WINPNONE8))
-//  if (defined(GD_PLATFORM_OS_X) || defined(GD_PLATFORM_IOS))
-		GDINL Int64 Increment() { return Int64(::InterlockedIncrement64(static_cast<LONGLONG volatile*>(&self->Value))); }
-		GDINL Int64 Decrement() { return Int64(::InterlockedDecrement64(static_cast<LONGLONG volatile*>(&self->Value))); }
-		GDINL Int64 Exchange(Int64 const NewValue) { return Int64(::InterlockedExchange64(static_cast<LONGLONG volatile*>(&self->Value), LONGLONG(NewValue))); }
-#	elif (defined(GD_PLATFORM_OS_X) || defined(GD_PLATFORM_IOS))
-//	  if (defined(GD_PLATFORM_WINDOWS) || defined(GD_PLATFORM_XBOX_ONE) || defined(GD_PLATFORM_WINPNONE8))
-		GDINL Int64 Increment() { return Int64(::OSAtomicIncrement64Barrier(static_cast<int64_t volatile*>(&self->Value))); }
-		GDINL Int64 Decrement() { return Int64(::OSAtomicDecrement64Barrier(static_cast<int64_t volatile*>(&self->Value))); }
-		GDINL Int64 Exchange(Int64 const NewValue) 
-		{
-			Int64 ResultValue;
-			do  { ResultValue = *self->Value; } while (!OSAtomicCompareAndSwap64Barrier(RetVal, NewValue, static_cast<int64_t*>(&self->Value)));
-			return ResultValue;
-		}
-#	else
-#		error Atomic64 class is not implemented for this platform.
-#	endif	//   if (defined(GD_PLATFORM_WINDOWS) || defined(GD_PLATFORM_XBOX_ONE) || defined(GD_PLATFORM_WINPNONE8))
-			// elif (defined(GD_PLATFORM_OS_X) || defined(GD_PLATFORM_IOS))
-#endif	// if (defined(GD_DOCUMENTATION))
-	};	// class Atomic64
+GD_NAMESPACE_BEGIN
+
+	/// Includes stuff to provide atomic operations.
+	namespace Atomics
+	{
+		/// Atomically increments pointer value.
+		/// @param   Value Pointer to value that would be incremented.
+		/// @returns Incremented value.
+		GDAPI static Int32 InterlockedIncrement(Int32 volatile* const Value);
+		GDAPI static Int64 InterlockedIncrement(Int64 volatile* const Value);
+
+		/// Atomically decrements pointer value.
+		/// @param   Value Pointer to value that would be decremented.
+		/// @returns Decremented value.
+		GDAPI static Int32 InterlockedDecrement(Int32 volatile* const Value);
+		GDAPI static Int64 InterlockedDecrement(Int64 volatile* const Value);
+
+		/// Atomically add specified amount to pointer value.
+		/// @param   Value  Pointer to value that would be adjusted.
+		/// @param   Amount Amount that would be added to pointer value.
+		/// @returns Adjusted value.
+		GDAPI static Int32 InterlockedAdd(Int32 volatile* const Value, Int32 const Amount);
+		GDAPI static Int64 InterlockedAdd(Int64 volatile* const Value, Int64 const Amount);
+
+		/// Atomically swaps pointer value to new specified one and returns original pointer value.
+		/// @param   Value Pointer to value that would be swapped.
+		/// @param   Other New pointer value.
+		/// @returns Original value of pointer before it was swapped.
+		GDAPI static Int32 InterlockedExchange(Int32 volatile* const Value, Int32 const Other);
+		GDAPI static Int64 InterlockedExchange(Int64 volatile* const Value, Int64 const Other);
+
+		/// Atomically swaps pointer value to new specified one and returns original pointer value.
+		/// @param   Value Pointer to value that would be swapped.
+		/// @param   Other New pointer value.
+		/// @returns Original value of pointer before it was swapped.
+		GDAPI static handle InterlockedExchangePtr(handle* const Destination, handle const Other);
+
+		/// Atomically compares value to comparand and swaps to new specified one if they are equal and returns the original value.
+		/// @param   Value     Pointer to value that would be swapped.
+		/// @param   Comparand Value to which original pointer value would be compared with.
+		/// @param   Other     New pointer value.
+		/// @returns Original value of pointer before it was swapped.
+		GDAPI static Int32 InterlockedCompareExchange(Int32 volatile* const Destination, Int32 const Other, Int32 const Comparand);
+		GDAPI static Int64 InterlockedCompareExchange(Int64 volatile* const Destination, Int64 const Other, Int64 const Comparand);
+
+		/// Atomically compares value to comparand and swaps to new specified one if they are equal and returns the original value.
+		/// @param   Value     Pointer to value that would be swapped.
+		/// @param   Comparand Value to which original pointer value would be compared with.
+		/// @param   Other     New pointer value.
+		/// @returns Original value of pointer before it was swapped.
+		GDINL static handle InterlockedCompareExchangePtr(handle* const Destination, handle const Other, handle const Comparand);
+	}	// namespace Atomics
 
 GD_NAMESPACE_END
-
+#else	// if (defined(GD_DOCUMENTATION))
+#	if (defined(GD_PLATFORM_WINDOWS) || defined(GD_PLATFORM_XBOX_ONE) || defined(GD_PLATFORM_WINPNONE8))
+#		include <GoddamnEngine/Core/Threading/Atomic/Atomic.WinApi.hh>
+//	endif	// if (defined(GD_PLATFORM_WINDOWS) || defined(GD_PLATFORM_XBOX_ONE) || defined(GD_PLATFORM_WINPNONE8))
+#	elif (defined(GD_PLATFORM_LINUX) || defined(GD_PLATFORM_PS4) || defined(GD_PLATFORM_ANDROID))
+#		include <GoddamnEngine/Core/Threading/Atomic/Atomic.Posix.hh>
+//	endif	// elif (defined(GD_PLATFORM_LINUX) || defined(GD_PLATFORM_ANDROID) || defined(GD_PLATFORM_PS4))
+#	elif (defined(GD_PLATFORM_OS_X) || defined(GD_PLATFORM_IOS))
+#		include <GoddamnEngine/Core/Threading/Atomic/Atomic.Cocoa.hh>
+//	endif	// if (defined(GD_PLATFORM_OS_X) || defined(GD_PLATFORM_IOS))
+#	else
+#		error 'Atomic64 class is not implemented for this platform.'
+#	endif
+#endif	// if (defined(GD_DOCUMENTATION))
 #endif	// ifndef GD_CORE_THREADING_ATOMIC
