e690ebe62bbc9a44ebfb40578b82717e610ac075
Oleg Butakov <butakovoleg@gmail.com>
2018-01-30 15:41:38 +0300

    Implemented UTF-8 encoder on Apple platforms

--- Source/Projects/GoddamnCore/Source/GoddamnEngine/Core/Misc/StringConv_UnitTests.cpp
+++ Source/Projects/GoddamnCore/Source/GoddamnEngine/Core/Misc/StringConv_UnitTests.cpp
@@ -17,7 +17,7 @@ GD_NAMESPACE_BEGIN
 
 	gd_testing_unit_test(StringConvEncode)
 	{
-		// Somthing mysterious text in Russian.
+		// Some mysterious text in Russian.
 		WideChar textInRussianWide[] = L"\x0413\x043E\x0432\x043D\x043E\x0020\x0436\x043E\x043F\x0430";
 		Char textInRussianUTF8[] = "\xD0\x93\xD0\xBE\xD0\xB2\xD0\xBD\xD0\xBE\x20\xD0\xB6\xD0\xBE\xD0\xBF\xD0\xB0";  // NOLINT
 		
